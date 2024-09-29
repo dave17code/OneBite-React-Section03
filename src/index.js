3a01bd118229802c1284877dfb4d89aefb87288a
dave17code <dave17code@gmail.com>
2024-09-29 12:39:16 +0900

    randomcolor

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-// const { add, sub} = require("./math")
-import multiply from "./math.js";
-import { add, sub } from "./math.js";
+// // const { add, sub} = require("./math")
+// import multiply from "./math.js";
+// import { add, sub } from "./math.js";
 
-console.log(add(1, 2));
-console.log(sub(1, 2))
-console.log(multiply(3, 3));
+// console.log(add(1, 2));
+// console.log(sub(1, 2))
+// console.log(multiply(3, 3));
+
+import randomColor from "randomcolor";
+const color = randomColor();
+console.log(color);
