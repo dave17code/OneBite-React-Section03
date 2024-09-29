abbf02f3a0af51aed9e33173bf0696050131e14b
dave17code <dave17code@gmail.com>
2024-09-29 11:41:12 +0900

    Node.js-ModuleSystem

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// const { add, sub} = require("./math")
+import multiply from "./math.js";
+import { add, sub } from "./math.js";
+
+console.log(add(1, 2));
+console.log(sub(1, 2))
+console.log(multiply(3, 3));
